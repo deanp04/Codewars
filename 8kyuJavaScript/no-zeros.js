5707a9c25f62fa340abfe68c780605c830c9416a
Dean Patterson <deanp04@gmail.com>
2023-08-19 23:08:14 -0400

    codewars

---
+++ 8kyuJavaScript/no-zeros.js
@@ -0,0 +1,20 @@
+// Numbers ending with zeros are boring.
+
+// They might be fun in your world, but not here.
+
+// Get rid of them. Only the ending ones.
+
+// 1450 -> 145
+// 960000 -> 96
+// 1050 -> 105
+// -1050 -> -105
+// Zero alone is fine, don't worry about it. Poor guy anyway
+
+// MY SOLUTION
+function noBoringZeros(n) {
+    let numberStr = String(n);
+    while (numberStr.endsWith("0")) {
+      numberStr = numberStr.slice(0, -1);
+    }
+    return Number(numberStr);
+  }
